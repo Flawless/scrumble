ee14c59ac9398d8d167e593c2ebfdd92c9cab37c
flawless <alushanov92@gmail.com>
2023-04-22 19:08:45 +0400

    Init project

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((clojure-mode
+  (cider-preferred-build-tool       . clojure-cli)
+  (cider-clojure-cli-global-options . "-A:dev:test")
+  (cider-default-cljs-repl          . custom)
+  (cider-custom-cljs-repl-init-form . "(do (user/cljs-repl))")
+  ;; (eval . (progn
+  ;;	    (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
+  ;;	    (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware")))
+  ))
